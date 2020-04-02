b1a6ae0177e1ea8e69f54a16f4d5baa77671bc52
rabah babaci <4rabah@gmail.com>
2020-04-02 15:46:28 -0700

    create a state the todo input

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import { v4 as id } from 'uuid';
 
 import { reducer, initialState } from '../reducers/reducer';
@@ -6,6 +6,7 @@ import Todo from './Todo';
 
 export default function TodoList() {
   const [todoList, dispatch] = useReducer(reducer, initialState);
+  const [todo, setTodo] = useState('');
 
   const addTodo = todo => {
     dispatch({
