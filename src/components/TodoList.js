de39876aed99a5b9873242b2967581405d1a4774
rabah babaci <4rabah@gmail.com>
2020-04-02 15:35:14 -0700

    map over the todoList and renders all items

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import { v4 as id } from 'uuid';
 
 import { reducer, initialState } from '../reducers/reducer';
 import Todo from './Todo';
@@ -9,6 +10,10 @@ export default function TodoList() {
   return (
     <div className='todo-list'>
       <h1>todo list</h1>
+
+      {todoList.map(todo => (
+        <Todo todo={todo} key={id()} />
+      ))}
     </div>
   );
 }
