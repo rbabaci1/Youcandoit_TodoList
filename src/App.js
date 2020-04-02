2e719d90f1843466ac1797d8f466c6daa0dccf6c
rabah babaci <4rabah@gmail.com>
2020-04-02 15:25:04 -0700

    import and render TodoList in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+
 function App() {
   return (
     <div className='App'>
-      <h1>Test</h1>
+      <TodoList />
     </div>
   );
 }
