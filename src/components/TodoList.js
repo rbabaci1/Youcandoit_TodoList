149c8b89b5dc66b23d06c91009abdc13d893fd86
rabah babaci <4rabah@gmail.com>
2020-04-02 15:33:21 -0700

    call the useReducer hook and pass the reducer and the initial state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,11 @@
 import React, { useReducer } from 'react';
 
+import { reducer, initialState } from '../reducers/reducer';
 import Todo from './Todo';
 
 export default function TodoList() {
+  const [todoList, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className='todo-list'>
       <h1>todo list</h1>
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,13 +1,17 @@
 import { v4 as id } from 'uuid';
 
-const initialState = {
-  item: 'Learn about reducers',
-  completed: false,
-  id: id()
-};
+const initialState = [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: id()
+  }
+];
+
+const ADD = 'ADD';
 
 const reducer = (currentState, action) => {
   return currentState;
 };
 
-export { initialState, reducer };
+export { reducer, initialState };
